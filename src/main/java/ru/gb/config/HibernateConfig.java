acc2f239eddfd4c838fefd21612ed327ebf8391c
Vlad <ivanovvlad.261996@mail.ru>
2021-12-05 04:51:51 +0300

    Added Homework, see Cart class, i added field totalCost and method toString in class Buyer he out sum all Carts.

--- src/main/java/ru/gb/config/HibernateConfig.java
+++ src/main/java/ru/gb/config/HibernateConfig.java
@@ -1,19 +1,17 @@
 package ru.gb.config;
 
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.Driver;
@@ -23,13 +21,12 @@ import java.util.Properties;
 @PropertySource("classpath:jdbc.properties")
 @ComponentScan("ru.gb")
 @EnableTransactionManagement
-public class JpaConfig {
-
+public class HibernateConfig {
     @Value("${driverClassName}")
     private String driverClassName;
     @Value("${url}")
     private String url;
-    @Value("${username}")
+    @Value("${pguser}")
     private String username;
     @Value("${password}")
     private String password;
@@ -50,36 +47,30 @@ public class JpaConfig {
         return null;
     }
 
+    @Bean
+    public SessionFactory sessionFactory() throws IOException {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+        sessionFactoryBean.setPackagesToScan("ru.gb");
+        sessionFactoryBean.setHibernateProperties(hibernateProperties());
+        sessionFactoryBean.afterPropertiesSet();
+        return sessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() throws IOException {
+        return new HibernateTransactionManager(sessionFactory());
+    }
+
     private Properties hibernateProperties() {
         Properties hibernateProperties = new Properties();
         hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.ProgressDialect");
         hibernateProperties.put("hibernate.format_sql", true);
         hibernateProperties.put("hibernate.use_sql_comments", true);
-        hibernateProperties.put("hibernate.show_sql", true);
+//        hibernateProperties.put("hibernate.show_sql", true);
         hibernateProperties.put("hibernate.max_fetch_depth", 3);
         hibernateProperties.put("hibernate.jdbc.batch_size", 10);
         hibernateProperties.put("hibernate.fetch_size", 50);
         return hibernateProperties;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager() throws IOException {
-        return new JpaTransactionManager();
-    }
-
-    @Bean
-    public EntityManagerFactory entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
-        factoryBean.setPackagesToScan("ru.gb.entity");
-        factoryBean.setDataSource(dataSource());
-        factoryBean.setJpaVendorAdapter(jpaVendorAdapter());
-        factoryBean.setJpaProperties(hibernateProperties());
-        factoryBean.afterPropertiesSet();
-        return factoryBean.getNativeEntityManagerFactory();
-    }
-
-    @Bean
-    public JpaVendorAdapter jpaVendorAdapter() {
-        return new HibernateJpaVendorAdapter();
     }
 }
